84960bc1e23d11a7d09899351dd9770372a99aa5
dnitra <109307365+dnitra@users.noreply.github.com>
2022-09-26 16:59:07 +0200

    added shuffleCards function

--- src/js/script.js
+++ src/js/script.js
@@ -3,12 +3,15 @@
 const cardsArr = ["url1", "url2", "url3", "..."];
 // console.log(cardsArr);
 // Shuffle cards array
-const shuffle = () => {
-  cardsArr.forEach((url, i) => {
-    arr.push(url[Math.random()]);
-  });
-};
-shuffle();
+const shuffleCards = (array)=> {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+shuffleCards(cardsArr)
+
 
 // Card flipping function
 // card.addEventListener("click", () => {
