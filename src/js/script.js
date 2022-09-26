4ee8a28886d45218a594401889b4ba283f4645ec
KubaGithub92 <jakub.rosol@email.cz>
2022-09-26 16:39:51 +0200

    created flipping function after click, started creating shuffle array function

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+// Selecting elements
+const cardsArr = ["url1", "url2", "url3", "..."];
+console.log(cardsArr);
+// Shuffle cards array
+const shuffle = () => {
+  cardsArr.forEach((url, i) => )
+};
+shuffle();
+
+// Card flipping function
+// card.addEventListener("click", () => {
+//   const card = document.querySelector(".pexesos__card");
+//   card.classList.add("card_flipped");
+// });
